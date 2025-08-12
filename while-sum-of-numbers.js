c7e462909d7e048b50010e8e4519a1ec0587ddfb
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-12 22:38:17 +0600

    added while-sum-of-numbers.js file

---
+++ while-sum-of-numbers.js
@@ -0,0 +1,24 @@
+/* Task-03 */
+
+// Subtask-1
+let i = 81;
+let sum = 0;
+while (i <= 131) {
+    if (i % 2 === 1) {
+        sum += i;
+    }
+    i++;
+}
+console.log("Sum of the odd numbers:", sum);
+
+// Subtask-2
+let n = 206;
+let sum2 = 0;
+while (n <= 311) {
+    if (n % 2 === 0) {
+        console.log(n)
+        sum2 += n;
+    }
+    n++;
+}
+console.log("Sum of the even numbers:", sum2);
