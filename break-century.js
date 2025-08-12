bc6bfb0ee7c4be5e272696b35aff81128ee434e8
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-13 02:23:07 +0600

    added break-century.js and '.\break\1 century.md'

---
+++ break-century.js
@@ -0,0 +1,20 @@
+/* Task-01 */
+// Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+// while loop
+let i = 1;
+while (i <= 200) {
+    console.log(i);
+    if (i === 100) {
+        break;
+    }
+    i++;
+}
+
+// for loop
+for (let i = 1; i <= 200; i++) {
+    console.log(i);
+    if (i === 100) {
+        break;
+    }
+}
