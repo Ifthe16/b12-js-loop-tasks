b399eba0632fca0780d992ba021589e6792fea4a
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-12 23:42:15 +0600

    added for-odd-even.js and 2 odd even.md file

---
+++ for-odd-even.js
@@ -0,0 +1,15 @@
+/* Task-02 */
+
+// Subtask-1
+for (let i = 61; i <= 100; i++) {
+    if (i % 2 === 1) {
+        console.log('Odd:', i);
+    }
+}
+
+// Subtask-2
+for (let i = 78; i <= 98; i++) {
+    if (i % 2 === 0) {
+        console.log('Even:', i);
+    }
+}
