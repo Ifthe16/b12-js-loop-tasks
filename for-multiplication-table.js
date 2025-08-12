45b2f9f95b1aac03f01bd385c1c6165f3f2fa7ea
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-13 00:05:26 +0600

    added 4 multiplication table.md' and for-multiplication-table.js

---
+++ for-multiplication-table.js
@@ -0,0 +1,6 @@
+/* Task-04 */
+let n = 9;
+for (let i = 1; i <= 10; i++) {
+    let multiply = n * i;
+    console.log(`${n} x ${i} = ${multiply}`);
+}
---
+++ for/4 multiplication table.md
@@ -0,0 +1,6 @@
+## Task-04
+
+Generate a multiplication table for number 9.
+
+
+*programming hero*
