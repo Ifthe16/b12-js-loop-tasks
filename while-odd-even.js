b059b1bd0c6bdeb85e26b2601a6b3706fbdde7d8
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-12 22:15:01 +0600

    added while-odd-even.js and updated while-commitment.js file

---
+++ while-odd-even.js
@@ -0,0 +1,19 @@
+/* Task-02 */
+
+// Subtask-1
+let i = 61;
+while (i <= 100) {
+    if (i % 2 === 1) {
+        console.log('Odd:', i);
+    }
+    i++;
+}
+
+// Subtask-2
+let n = 78;
+while (n <= 98) {
+    if (n % 2 === 0) {
+        console.log("Even:", n);
+    }
+    n++;
+}
