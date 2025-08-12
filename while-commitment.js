93286aa6a3111ea183d510c3af8f317e3b973080
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-12 22:16:45 +0600

    updated while-commitment.js file

--- while-commitment.js
+++ while-commitment.js
@@ -1,6 +1,6 @@
+/* Task-01 */
 let i = 1;
 while (i <= 60) {
-    console.log(i,"I will invest at least 6 hrs every single day for next 60 days!");
+    console.log(i, "I will invest at least 6 hrs every single day for next 60 days!");
     i++;
-
 }
