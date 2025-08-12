1eaacbee48d33268499622188469bf7be3fdb432
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-13 00:01:57 +0600

    updated while-multiplication-table.js file

--- while-multiplication-table.js
+++ while-multiplication-table.js
@@ -1,9 +1,8 @@
 /* Task-04 */
 let n = 1;
 let i = 5;
-let multiply = 0;
 while (n <= 10) {
-    multiply = n * i;
+    let multiply = n * i;
     console.log(i, 'x', n, '=', multiply);
     n++;
 }
